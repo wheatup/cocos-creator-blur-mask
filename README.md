54f5002fe95093c7b34ed14a58cfed865d841893
wheatup <acphun@gmail.com>
2019-05-10 18:17:12 +0900

    更新算法,支持排除渲染对象和部分渲染

--- README.md
+++ README.md
@@ -9,3 +9,9 @@
 ### 效果
 
 ![[效果](https://i.imgur.com/TGXDHlq.gif)](https://i.imgur.com/TGXDHlq.gif)
+
+## 2019-05-10 更新
+
+* 支持渲染排除，可以排除不想渲染的对象。
+
+* 支持只显示部分区域，超出节点box外的背景将不会受影响。
--- assets/scripts/BlurMask.ts
+++ assets/scripts/BlurMask.ts
@@ -12,6 +12,9 @@ export default class BlurMask extends cc.Component {
 	// @ts-ignore
 	@property(cc.Material) material: cc.Material = null;
 
+	@property([cc.Node])
+	ignoredNodes = [];
+
 	start() {
 		// 截图图像是翻转的，所以y轴镜像
 		this.node.scaleY = -1;
@@ -22,30 +25,29 @@ export default class BlurMask extends cc.Component {
 
 		// 在node上创建摄影机
 		this.camera = this.node.addComponent(cc.Camera);
-		this.camera.cullingMask = 0xffffffff;
+		// 不渲染0x10的cullingMask对象
+		this.camera.cullingMask = 0xffffffff ^ 0x10000000;
 		this.camera.targetTexture = this.texture;
 		// 关闭摄影机，否则每一帧它会自动进行渲染
 		this.camera.enabled = false;
+
+		// 将自身与忽略对象排除渲染
+		this.node['_cullingMask'] = 0x10000000;
+		this.ignoredNodes.map(node => node['_cullingMask'] = 0x10000000);
 
 		// 创建一个sprite组件，由其进行渲染
 		this.spriteFrame = new cc.SpriteFrame();
 		this.sprite = this.node.addComponent(cc.Sprite);
 		this.sprite.spriteFrame = this.spriteFrame;
-		// @ts-ignore
-		this.sprite._materials[0] = this.material;
+		this.sprite['_materials'][0] = this.material;
 	}
 
 	// 截图并模糊
 	snapshot() {
-		let active = this.node.active;
-		// 截图前隐藏当前层
-		this.node.active = false;
 		// 手动渲染摄影机，保存截图
 		this.camera.render(cc.Canvas.instance.node);
 		// 应用刚刚截图的贴图到sprite身上进行渲染
-		this.spriteFrame.setTexture(this.texture);
-		// 还原当前层显示
-		this.node.active = active;
+		this.spriteFrame.setTexture(this.texture, this.node.getBoundingBoxToWorld());
 	}
 
 	update(dt) {
